5f5e48aba8ec13f26d7b33d01e8f2196600ef389
Florian Hirsch <fhi@adorsys.de>
2014-11-02 17:50:06 +0100

    scrolling + form validation

---
+++ scripts/ruppertdesign.js
@@ -0,0 +1,80 @@
+(function () {
+    
+	var init = function() {
+		registerScrolling();
+		registerContactSubmit();
+	};
+
+	var registerScrolling = function() {
+		$('[href="#kontakt"]').on('click', function(e) {
+			window.smoothScroll($('#kontakt')[0], 1500);
+		});
+	};
+
+	var registerContactSubmit = function() {
+		var form = $('#contactForm');
+		form.attr('novalidate', 'novalidate');
+		form.on('submit', function(e) {
+			e.preventDefault();
+			if (!validateContactForm(form)) {
+				return false;
+			}
+			var data = {};
+			form.serializeArray().map(function(o) {data[o.name] = o.value;}); 
+			data['newsletter'] = $('#newsletter').is(':checked') ? 'Ja' : 'Nein';
+
+			$.ajax({
+			    url: form.attr('action'), 
+			    type: 'POST',
+			    data: data,
+			    dataType: 'json',
+			    success: function(data) {
+			    	$(form).fadeOut(150, function() {
+			    		$('#submitSuccess').fadeIn(250);	
+			    	});
+				  },
+				  error: function(xhr, type) {
+				  	var errorMsg = $('#submitError');
+				    errorMsg.fadeIn(250);
+				    window.smoothScroll(errorMsg[0], 250);
+				  }
+			});
+
+
+		});
+	};
+
+	var validateContactForm = function(form) {
+		$('#submitSuccess, #submitError').fadeOut(150);
+		var valid = true;
+		$('input[type=text], input[type=email], textarea', form).each(function() {
+			var field = $(this)[0];
+			if (typeof field.willValidate !== 'undefined') {
+        field.checkValidity();
+      } else {
+      	field.validity = field.validity || {};
+			  field.validity.valid = validate($(this));
+      }
+      if (field.validity.valid) {
+ 				$(this).removeClass('error').next('.error-msg').fadeOut(150);
+			} else {
+ 				$(this).addClass('error').next('.error-msg').fadeIn(150);
+        valid = false;
+      }
+		});
+		return valid;
+	};
+
+	var validate = function(field) {
+		var valid = true;
+		var val = field.val().trim();
+		valid = valid && field.attr('required') ? val != '' : true;
+		valid = valid && field.attr('minlength') ? val >= field.attr('minlength') : true;
+		valid = valid && field.attr('maxlength') ? val >= field.attr('maxlength') : true;
+		valid = valid && field.attr('pattern') ? new RegExp('^(?:' + field.attr('pattern') + ')$').test(val) : true;
+		return valid;
+	};
+
+	$(document).ready(init);
+
+})();
