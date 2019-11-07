7521dece62351191ef39e41051aebce74bd71e31
Florian Hirsch <post@florianhirsch.de>
2019-11-07 22:24:43 +0100

    Hide unused components

--- src/pages/Configuration.jsx
+++ src/pages/Configuration.jsx
@@ -24,82 +24,90 @@ export default ({ formValues, setFormValue }) => (
       value={formValues['Holzart']}
       setFormValue={setFormValue}
     />
-    <ConfigurationRow
-      title="Bitte wählen Sie die Größe"
-      name="Größe"
-      entries={[
-        {
-          value: 'Klein',
-          description: 'Maße',
-          image: '/schluesselanhaenger/img/konfigurator/klein.jpg',
-        },
-        {
-          value: 'Groß',
-          description: 'Maße',
-          image: '/schluesselanhaenger/img/konfigurator/gross.jpg',
-        },
-      ]}
-      value={formValues['Größe']}
-      setFormValue={setFormValue}
-    />
-    <ConfigurationRow
-      title="Bitte wählen Sie die das Band"
-      name="Band"
-      entries={[
-        {
-          value: 'Band 1',
-          description: 'Beschreibunng 1',
-          image: '/schluesselanhaenger/img/konfigurator/band1.jpg',
-        },
-        {
-          value: 'Band 2',
-          description: 'Beschreibunng 2',
-          image: '/schluesselanhaenger/img/konfigurator/band2.jpg',
-        },
-        {
-          value: 'Band 3',
-          description: 'Beschreibunng 3',
-          image: '/schluesselanhaenger/img/konfigurator/band3.jpg',
-        },
-      ]}
-      value={formValues['Band']}
-      setFormValue={setFormValue}
-    />
-    <ConfigurationRow
-      title="Bitte wählen Sie die Schriftart"
-      name="Schriftart"
-      entries={[
-        {
-          value: 'Schriftart 1',
-          description: 'Beschreibunng 1',
-          image: '/schluesselanhaenger/img/konfigurator/band1.jpg',
-        },
-        {
-          value: 'Schriftart 2',
-          description: 'Beschreibunng 2',
-          image: '/schluesselanhaenger/img/konfigurator/band2.jpg',
-        },
-        {
-          value: 'Schriftart 3',
-          description: 'Beschreibunng 3',
-          image: '/schluesselanhaenger/img/konfigurator/band3.jpg',
-        },
-        {
-          value: 'Schriftart 4',
-          description: 'Beschreibunng 4',
-          image: '/schluesselanhaenger/img/konfigurator/band1.jpg',
-        },
-        {
-          value: 'Schriftart 5',
-          description: 'Beschreibunng 5',
-          image: '/schluesselanhaenger/img/konfigurator/band2.jpg',
-        },
-      ]}
-      value={formValues['Schriftart']}
-      setFormValue={setFormValue}
-    />
-    <a class="pure-button" href="#adresse">
-      Weiter
-    </a>
+    {formValues['Holzart'] != null && (
+      <ConfigurationRow
+        title="Bitte wählen Sie die Größe"
+        name="Größe"
+        entries={[
+          {
+            value: 'Klein',
+            description: 'Maße',
+            image: '/schluesselanhaenger/img/konfigurator/klein.jpg',
+          },
+          {
+            value: 'Groß',
+            description: 'Maße',
+            image: '/schluesselanhaenger/img/konfigurator/gross.jpg',
+          },
+        ]}
+        value={formValues['Größe']}
+        setFormValue={setFormValue}
+      />
+    )}
+    {formValues['Größe'] != null && (
+      <ConfigurationRow
+        title="Bitte wählen Sie die das Band"
+        name="Band"
+        entries={[
+          {
+            value: 'Band 1',
+            description: 'Beschreibunng 1',
+            image: '/schluesselanhaenger/img/konfigurator/band1.jpg',
+          },
+          {
+            value: 'Band 2',
+            description: 'Beschreibunng 2',
+            image: '/schluesselanhaenger/img/konfigurator/band2.jpg',
+          },
+          {
+            value: 'Band 3',
+            description: 'Beschreibunng 3',
+            image: '/schluesselanhaenger/img/konfigurator/band3.jpg',
+          },
+        ]}
+        value={formValues['Band']}
+        setFormValue={setFormValue}
+      />
+    )}
+    {formValues['Band'] != null && (
+      <ConfigurationRow
+        title="Bitte wählen Sie die Schriftart"
+        name="Schriftart"
+        entries={[
+          {
+            value: 'Schriftart 1',
+            description: 'Beschreibunng 1',
+            image: '/schluesselanhaenger/img/konfigurator/band1.jpg',
+          },
+          {
+            value: 'Schriftart 2',
+            description: 'Beschreibunng 2',
+            image: '/schluesselanhaenger/img/konfigurator/band2.jpg',
+          },
+          {
+            value: 'Schriftart 3',
+            description: 'Beschreibunng 3',
+            image: '/schluesselanhaenger/img/konfigurator/band3.jpg',
+          },
+          {
+            value: 'Schriftart 4',
+            description: 'Beschreibunng 4',
+            image: '/schluesselanhaenger/img/konfigurator/band1.jpg',
+          },
+          {
+            value: 'Schriftart 5',
+            description: 'Beschreibunng 5',
+            image: '/schluesselanhaenger/img/konfigurator/band2.jpg',
+          },
+        ]}
+        value={formValues['Schriftart']}
+        setFormValue={setFormValue}
+      />
+    )}
+    {formValues['Schriftart'] != null && (
+      <a class="pure-button" href="#adresse">
+        Weiter
+      </a>
+    )}
   </Fragment>
 )
