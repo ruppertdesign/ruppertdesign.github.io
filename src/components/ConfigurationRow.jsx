1b99c9dc48a750a6c13bca68aea7e57bae69063e
Florian Hirsch <post@florianhirsch.de>
2019-11-10 07:50:27 +0100

    Scroll to next row

--- src/components/ConfigurationRow.jsx
+++ src/components/ConfigurationRow.jsx
@@ -1,36 +1,52 @@
-import { h } from 'preact'
+import { h, Component, createRef } from 'preact'
 
-export default ({ title, name, entries, value, setFormValue }) => (
-  <fieldset>
-    <legend>{title}</legend>
-    {entries.map(entry => {
-      const cols = entries.length < 3 ? 2 : 3
-      return (
-        <div
-          key={`${name}-${entry.value}`}
-          class={`pure-u-1-1 pure-u-md-1-${cols} pure-u-lg-1-${cols} configurator-box`}
-        >
-          <div class="img-wrapper">
-            <label>
-              <input
-                type="radio"
-                name={name}
-                value={entry.value}
-                checked={entry.value == value}
-                onClick={e => setFormValue(name, e.target.value)}
-              />
-              <img
-                src={entry.image}
-                width={320}
-                class="pure-img bw"
-                title={name}
-              />
-            </label>
-          </div>
-          <h3>{entry.value}</h3>
-          <p>{entry.description}</p>
-        </div>
-      )
-    })}
-  </fieldset>
-)
+export default class ConfigurationRow extends Component {
+  ref = createRef()
+
+  handleSelect = event => {
+    const { name, setFormValue } = this.props
+    setFormValue(name, event.target.value)
+    const fieldset = this.ref.current
+    window.scrollTo({
+      top: fieldset.offsetTop + fieldset.offsetHeight,
+      behavior: 'smooth',
+    })
+  }
+
+  render({ title, name, entries, value }) {
+    return (
+      <fieldset ref={this.ref}>
+        <legend>{title}</legend>
+        {entries.map(entry => {
+          const cols = entries.length < 3 ? 2 : 3
+          return (
+            <div
+              key={`${name}-${entry.value}`}
+              class={`pure-u-1-1 pure-u-md-1-${cols} pure-u-lg-1-${cols} configurator-box`}
+            >
+              <div class="img-wrapper">
+                <label>
+                  <input
+                    type="radio"
+                    name={name}
+                    value={entry.value}
+                    checked={entry.value == value}
+                    onClick={this.handleSelect}
+                  />
+                  <img
+                    src={entry.image}
+                    width={320}
+                    class="pure-img bw"
+                    title={name}
+                  />
+                </label>
+              </div>
+              <h3>{entry.value}</h3>
+              <p>{entry.description}</p>
+            </div>
+          )
+        })}
+      </fieldset>
+    )
+  }
+}
