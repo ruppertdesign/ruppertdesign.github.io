e0ee8bf618c01ae6f5a0b1f082f0c742b69b3ecf
Florian Hirsch <post@florianhirsch.de>
2020-09-22 19:16:34 +0200

    Add address form

--- src/pages/ShippingAndPayment.jsx
+++ src/pages/ShippingAndPayment.jsx
@@ -1,11 +1,68 @@
 import { h, Fragment } from 'preact'
 import Header from '../components/Header'
+import InputText from '../components/InputText'
+import InputTextarea from '../components/InputTextarea'
 
-export default () => (
+export default ({ formValues, setFormValue, validateForm, navigate }) => (
   <Fragment>
-    <Header title="Adresse und Bezahlung" />
-    <a class="pure-button" href="#auswahl">
-      Zurück
-    </a>
+    <form
+      name="shippingAndPayment"
+      class="pure-form pure-form-stacked configurator"
+      onSubmit={(event) => {
+        event.preventDefault()
+        if (validateForm(event)) {
+          navigate('#danke')
+        }
+      }}
+      noValidate
+    >
+      <Header title="Adresse und Bezahlung" />
+      <fieldset class="label-fields">
+        <legend>Lieferadresse</legend>
+        <div class="pure-g">
+          <div class="pure-u-1-1 pure-u-md-1-2 pure-u-lg-1-2">
+            <InputText
+              name="name"
+              title="Name"
+              formValue={formValues.name}
+              setFormValue={setFormValue}
+              required
+              maxlength={60}
+            />
+            <InputText
+              name="street"
+              title="Straße und Hausnummer"
+              formValue={formValues.name}
+              setFormValue={setFormValue}
+              required
+              maxlength={60}
+            />
+            <div class="pure-u-1 pure-u-md-1-3">
+              <div class="postalcode">
+                <InputText
+                  name="street"
+                  title="Postleitzahl"
+                  formValue={formValues.name}
+                  setFormValue={setFormValue}
+                  required
+                  maxlength={5}
+                />
+              </div>
+            </div>
+            <div class="pure-u-1 pure-u-md-2-3">
+              <InputText
+                name="street"
+                title="Ort"
+                formValue={formValues.name}
+                setFormValue={setFormValue}
+                required
+                maxlength={60}
+              />
+            </div>
+          </div>
+        </div>
+      </fieldset>
+      <p>Hier fehlt die Erklärung wie es mit der Bezahlung läuft</p>
+    </form>
   </Fragment>
 )
