35f22457c1574fe522685a8b637dd49655d9990f
Florian Hirsch <post@florianhirsch.de>
2021-05-03 07:28:01 +0200

    Add new yeah img

--- src/pages/Thanks.jsx
+++ src/pages/Thanks.jsx
@@ -5,7 +5,14 @@ export default () => (
   <Fragment>
     <Header>Danke für Ihre Bestellung</Header>
     <div class="center">
-      <img class="small" src="/konfigurator/img/yeah.jpg" alt="Yeah!" />
+      <img
+        class="small"
+        src="/konfigurator/img/yeah.jpg"
+        srcset="/konfigurator/img/yeah.jpg 1x,
+        /konfigurator/img/yeah@2x.jpg 2x"
+        width="640"
+        alt="Yeah!"
+      />
       <p>
         Wir freuen uns, dass wir Ihren Wunschanhänger für Sie fertigen dürfen.
       </p>
